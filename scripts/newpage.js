1d3096bc9c4f7c27c3fd120dacbc056f4b1fecfa
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-03-30 21:25:45 +0530

    newpage changes
    ..

--- scripts/newpage.js
+++ scripts/newpage.js
@@ -17,6 +17,17 @@ const getData= async (url)=>{
 const appendData = (data,main) => {
     data.forEach((elem)=>{
         let div = document.createElement('div');
+        let quicklook = document.createElement('div');
+        quicklook.id ="quicklook";
+        quicklook.style ="background-color:grey;opacity:0.5;margin:auto"
+        div.addEventListener("mouseover", ()=>{
+            quicklook.innerHTML =null ;
+            quicklook.innerHTML = "QuickLook";        
+            div.append(quicklook);
+        })
+        div.addEventListener("mouseout", ()=>{
+            quicklook.innerHTML = null ;
+        })
 
         let image1 = document.createElement('img');
         image1.src = elem.image ;
@@ -36,8 +47,25 @@ const appendData = (data,main) => {
 
         let price1 = document.createElement('h5');
         price1.innerText = elem.price ;
-
-        div.append(image1,brand1,title1,price1,)
+        let span = document.createElement('span');
+        span.style="display:flex;justify-content:space-between;width:20%;margin:auto"
+        let star1 = document.createElement('img');
+        star1.style = "height:6px; width:6px"
+        star1.src = "https://www.sephora.com/img/ufe/icons/star-outline.svg"
+        let star2 = document.createElement('img');
+        star2.style = "height:6px; width:6px"
+        star2.src = "https://www.sephora.com/img/ufe/icons/star-outline.svg"
+        let star3 = document.createElement('img');
+        star3.style = "height:6px; width:6px"
+        star3.src = "https://www.sephora.com/img/ufe/icons/star-outline.svg"
+        let star4 = document.createElement('img');
+        star4.style = "height:6px; width:6px"
+        star4.src = "https://www.sephora.com/img/ufe/icons/star-outline.svg"
+        let star5 = document.createElement('img');
+        star5.style = "height:6px; width:6px"
+        star5.src = "https://www.sephora.com/img/ufe/icons/star-outline.svg"
+        span.append(star1,star2,star3,star4,star5)
+        div.append(image1,brand1,title1,price1,span)
 
         main.append(div);
 
