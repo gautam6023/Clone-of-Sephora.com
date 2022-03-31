5cfb36a7e430691d16b485faec6413651439063a
gautam6023 <gautamgohil0@gmail.com>
2022-03-31 21:23:49 +0530

    updated cart

--- script/cart.js
+++ script/cart.js
@@ -7,25 +7,30 @@ let cartData = JSON.parse(localStorage.getItem("cart")) || [];
 console.log(cartData);
 
 //catching container
-let cartProducts = document.getElementById("tbody");
+let cartProducts = document.getElementById("cartItemsTable");
 const displayData = (data) => {
   cartProducts.innerHTML = "";
   data.map((el) => {
     let { brand, title, image, price } = el;
 
-    let row = document.createElement("tr");
+    let row = document.createElement("div");
+    row.setAttribute("id", "row");
 
-    let col1 = document.createElement("td");
+    // let col1 = document.createElement("d");
 
     let img = document.createElement("img");
     img.src = image;
+    img.setAttribute("class", "cartImage");
 
-    col1.append(img);
+    // col1.append(img);
 
-    let col2 = document.createElement("td");
+    let col2 = document.createElement("div");
+    col2.setAttribute("id", "secondDiv");
 
+    let innDiv = document.createElement("div");
     let Title = document.createElement("p");
     Title.innerText = title;
+    Title.setAttribute("id", "cartTitle");
 
     let desc = document.createElement("p");
     desc.innerText = brand;
@@ -33,9 +38,12 @@ const displayData = (data) => {
     let itemCode = document.createElement("p");
     let code = Math.floor(Math.random() * (1000000 - 500000 + 1)) + 500000;
     itemCode.innerText = `ITEM ${code}`;
+    itemCode.style.color = "#666666";
+    itemCode.style.fontSize = "1.3vh";
 
+    innDiv.append(Title, desc, itemCode);
     let div = document.createElement("div");
-
+    div.setAttribute("id", "btnForCart");
     let select = document.createElement("select");
     select.innerHTML = `<option value="1">1</option>
     <option value="2">2</option>
@@ -54,18 +62,22 @@ const displayData = (data) => {
     let btnRemove = document.createElement("button");
     btnRemove.innerText = `Remove`;
 
+    let hr = document.createElement("div");
+    hr.setAttribute("id", "headerLine2");
+
     div.append(select, btnLoves, btnRemove);
 
     //Added col two elements
-    col2.append(Title, desc, itemCode, div);
+    col2.append(innDiv, div);
 
-    let col3 = document.createElement("td");
+    let col3 = document.createElement("div");
+    col3.setAttribute("id", "thirdDiv");
 
     let Price = document.createElement("p");
     Price.innerText = price;
     col3.append(price);
 
-    row.append(col1, col2, col3);
+    row.append(img, col2, col3, hr);
 
     cartProducts.append(row);
   });
