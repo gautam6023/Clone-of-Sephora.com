84d82c8974c4ed919ddfa93f7a6f0e4baf573120
gautam6023 <gautamgohil0@gmail.com>
2022-04-02 14:07:22 +0530

    minor change

--- components/Displaybrands.js
+++ components/Displaybrands.js
@@ -1,30 +1,39 @@
-import {displayAtoZ}  from "../brand.js";
+import { displayAtoZ } from "../brand.js";
 import { alphabeticalBrands } from "../Constants/constants.js";
-const Displaybrands=()=>{   
-    const alphabets =displayAtoZ();
-    console.log(alphabets)
-    const displayNew=isNew=>{
-        return isNew?`
+const Displaybrands = () => {
+  const alphabets = displayAtoZ();
+  console.log(alphabets);
+  const displayNew = (isNew) => {
+    return isNew
+      ? `
         <span class="isNew">NEW</span>
-        `:""
-    }
-    const displayBrandsAlphabetical=(alphabet)=>{
-    
-        const brands=alphabeticalBrands[alphabet];
-        console.log(brands);
-        return (brands||[]).map(res => {
-            return `<li><a class="brandList" href=${res.url}>${res.brand}${displayNew(res.isNew)}</a></li>`
-        }).join("")
-    }
-    const displayAlphabets=()=>alphabets.map(res=>{
-        return (alphabeticalBrands[res]||[]).length?`<div id=${res} class="gridBrands">
+        `
+      : "";
+  };
+  const displayBrandsAlphabetical = (alphabet) => {
+    const brands = alphabeticalBrands[alphabet];
+    console.log(brands);
+    return (brands || [])
+      .map((res) => {
+        return `<li><a class="brandList" >${res.brand}${displayNew(
+          res.isNew
+        )}</a></li>`;
+      })
+      .join("");
+  };
+  const displayAlphabets = () =>
+    alphabets
+      .map((res) => {
+        return (alphabeticalBrands[res] || []).length
+          ? `<div id=${res} class="gridBrands">
         <div class="alphabet">${res}</div>
         <div><ul class="gridList">${displayBrandsAlphabetical(res)}</ul></div>
         </div>    <div class="divider"></div>
-        `:""
-    }).join("")
-    return `<div>${displayAlphabets()}</div>`
- }
+        `
+          : "";
+      })
+      .join("");
+  return `<div>${displayAlphabets()}</div>`;
+};
 
- export default Displaybrands;
- 
+export default Displaybrands;
