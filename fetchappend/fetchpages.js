04532e1f884430dcd1f4def07f2f0b69dcbd75ab
Dathuram16 <99670603+Dathuram16@users.noreply.github.com>
2022-04-03 11:36:50 +0530

    Functionality in makeup

--- fetchappend/fetchpages.js
+++ fetchappend/fetchpages.js
@@ -8,40 +8,72 @@ const getData = async (url) => {
     //    productsarray.push(data);
     //    localStorage.setItem("products",JSON.stringify(productsarray));
 
-    let l2h = document.getElementById("sort_l2h");
-    l2h.addEventListener("click", () => {
-      console.log(data);
-      data.sort(function (a, b) {
-        return a.currentSku.listPrice - b.currentSku.listPrice;
-      });
-      appendData(data, main);
-    });
+    // let l2h = document.getElementById("sort_l2h");
+    // l2h.addEventListener("click", () => {
+    //   console.log(data);
+    //   data.sort(function (a, b) {
+    //     return a.currentSku.listPrice - b.currentSku.listPrice;
+    //   });
+    //   appendData(data, main);
+    // });
 
-    let h2l = document.getElementById("sort_h2l");
-    h2l.addEventListener("click", () => {
-      console.log(data);
-      data.sort(function (a, b) {
-        return b.currentSku.listPrice - a.currentSku.listPrice;
-      });
-      appendData(data, main);
-    });
+    // let h2l = document.getElementById("sortSelect");
+    // h2l.addEventListener("change", () => {
+    //   console.log(data);
+    //   data.sort(function (a, b) {
+    //     return b.currentSku.listPrice - a.currentSku.listPrice;
+    //   });
+    //   appendData(data, main);
+    // });
 
-    let topRated = document.getElementById("topRated");
-    topRated.addEventListener("click", () => {
-      console.log(data);
-      data.sort(function (a, b) {
-        return b.rating - a.rating;
-      });
-      appendData(data, main);
-    });
 
-    //    var sorting = document.getElementById("sortdiv").value;
-    //    if(sorting===sort_h2l){
-    //        data.sort(function(a,b){
-    //             return b.currentSku.listPrice-a.currentSku.listPrice;
-    //         })
-    //         appendData(data,main) ;
-    //    }
+
+    // let topRated = document.getElementById("topRated");
+    // topRated.addEventListener("click", () => {
+    //   console.log(data);
+    //   data.sort(function (a, b) {
+    //     return b.rating - a.rating;
+    //   });
+    //   appendData(data, main);
+    // });
+
+      //  var sorting = document.getElementById("sortdiv").value;
+      //  if(sorting===sort_h2l){
+      //      data.sort(function(a,b){
+      //           return b.currentSku.listPrice-a.currentSku.listPrice;
+      //       })
+      //       appendData(data,main) ;
+      //  }
+
+      document.getElementById("sortSelect").addEventListener("change",()=>{
+        sortFun();
+      })
+
+      const sortFun =()=>{
+        let sortValue=document.getElementById("sortSelect").value
+        console.log(sortValue);
+        if(sortValue==="sort_h2l"){
+            data.sort(function(a,b){
+                 return b.currentSku.listPrice-a.currentSku.listPrice;
+             })
+             console.log(data);
+             appendData(data,main) ;
+        }
+        if(sortValue==="sort_l2h"){
+          data.sort(function(a,b){
+               return a.currentSku.listPrice-b.currentSku.listPrice;
+           })
+           console.log(data);
+           appendData(data,main) ;
+      }
+      if(sortValue==="topRated"){
+          data.sort(function (a, b) {
+                return b.rating - a.rating;
+              });
+         console.log(data);
+         appendData(data,main) ;
+    }
+      }
 
     return data;
   } catch (error) {
@@ -51,7 +83,7 @@ const getData = async (url) => {
 
 const appendData = (data, main) => {
   main.innerHTML = null;
-  data.forEach((elem) => {
+  data.forEach((elem,ind) => {
     let div = document.createElement("div");
     let quicklook = document.createElement("div");
     quicklook.id = "quicklook";
@@ -75,9 +107,12 @@ const appendData = (data, main) => {
     let heart = document.querySelector("#lovediv>svg");
 // >>>>>>> 2eb82d5cc326c4ccb3bf17750ddbc1da153e278c
     lovediv.addEventListener("click", () => {
-      console.log("1");
-        document.querySelector("#heart").style.fill = "red";
-        document.querySelector("#heart").style.stroke = "red";
+      console.log(ind);
+        document.querySelectorAll("#heart")[ind].style.fill = "red";
+        document.querySelectorAll("#heart")[ind].style.stroke = "red";
+        let wishArr= JSON.parse(localStorage.getItem("wish")) || [];
+        wishArr.push(elem);
+        localStorage.setItem("wish",JSON.stringify(wishArr));
     });
        
        
@@ -130,7 +165,7 @@ const appendData = (data, main) => {
     div.addEventListener("mouseleave", () => {
       quicklook.innerHTML = null;
       title1.style = "text-decoration: none";
-      lovediv.innerHTML = null ;
+      // lovediv.innerHTML = null ;
     });
 // <<<<< HEAD<<
     new_heart.append(newdiv, lovediv);
@@ -172,4 +207,6 @@ const appendData = (data, main) => {
   });
 };
 
-export { getData, appendData };
+
+
+export { getData, appendData};
